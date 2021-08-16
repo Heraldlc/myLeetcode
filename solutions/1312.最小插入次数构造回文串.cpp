da95c39435ba163c5a223c0a148e78bb2965bacd
Heraldlc <932172851@qq.com>
2021-08-16 17:42:01 +0800

    Create 1312.最小插入次数构造回文串.cpp

---
+++ solutions/1312.最小插入次数构造回文串.cpp
@@ -0,0 +1,31 @@
+/*
+动态规划
+同样dp[i][j]表示s[i~j]构造成回文串需要插入的次数
+那么base case就是i==j时，本身便不需要插入了，dp(i,i)=0
+
+如果s[i]==s[j],那么也不需要新的插入dp[i][j] = dp[i+1][j-1]
+如果s[i]!=s[j],比较麻烦
+- 我们需要先做选择，先将s[i~j-1]或者s[i+1~j]变成回文串，取代价较小的
+- 然后一定需要一次插入，插入s[i]或者s[j]
+dp[i][j] = min(dp[i][j-1], dp[i+1][j]) +1;
+*/
+
+class Solution {
+public:
+    int minInsertions(string s) {
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        for(int i = n-2; i>=0; i--)
+        {
+            for(int j=i+1; j<n; j++)
+            {
+                if(s[i] == s[j])
+                    dp[i][j] = dp[i+1][j-1];
+                else
+                    dp[i][j] = min(dp[i][j-1], dp[i+1][j]) +1;
+            }
+        }
+        return dp[0][n-1];
+    }
+};
