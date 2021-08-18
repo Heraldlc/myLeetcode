86ee50fcea4deeb13eb2a29ff42a6aee26dcae34
Heraldlc <932172851@qq.com>
2021-08-18 20:03:41 +0800

    Update 552.学生出勤次数Ⅱ.cpp

--- solutions/552.学生出勤次数Ⅱ.cpp
+++ solutions/552.学生出勤次数Ⅱ.cpp
@@ -50,3 +50,46 @@ public:
         return sum;
     }
 };
+
+// 还可以发现
+// dp(n, ...)的状态由dp(n-1,...)获得，所以可以状态压缩
+
+class Solution {
+public:
+    static constexpr int MOD = 1'000'000'007;
+
+    int checkRecord(int n) {
+        int dp[2][3];  // A 的数量，结尾连续 L 的数量
+        memset(dp, 0, sizeof(dp));
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            int dpNew[2][3];  // A 的数量，结尾连续 L 的数量
+            memset(dpNew, 0, sizeof(dpNew));
+            // 以 P 结尾的数量
+            for (int j = 0; j <= 1; j++) {
+                for (int k = 0; k <= 2; k++) {
+                    dpNew[j][0] = (dpNew[j][0] + dp[j][k]) % MOD;
+                }
+            }
+            // 以 A 结尾的数量
+            for (int k = 0; k <= 2; k++) {
+                dpNew[1][0] = (dpNew[1][0] + dp[0][k]) % MOD;
+            }
+            // 以 L 结尾的数量
+            for (int j = 0; j <= 1; j++) {
+                for (int k = 1; k <= 2; k++) {
+                    dpNew[j][k] = (dpNew[j][k] + dp[j][k - 1]) % MOD;
+                }
+            }
+            memcpy(dp, dpNew, sizeof(dp));
+        }
+        int sum = 0;
+        for (int j = 0; j <= 1; j++) {
+            for (int k = 0; k <= 2; k++) {
+                sum = (sum + dp[j][k]) % MOD;
+            }
+        }
+        return sum;
+    }
+};
+
