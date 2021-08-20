cb2e482b3d7a9e046fcbcc8c142511c45488898c
Heraldlc <932172851@qq.com>
2021-08-20 10:24:11 +0800

    Create 541.反转字符串Ⅱ.cpp

---
+++ solutions/541.反转字符串Ⅱ.cpp
@@ -0,0 +1,13 @@
+// 模拟题意即可
+
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        int n = s.length();
+        for (int i = 0; i < n; i += 2 * k) {
+            reverse(s.begin() + i, s.begin() + min(i + k, n));
+        }
+        return s;
+    }
+};
+
