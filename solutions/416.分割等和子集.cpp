4ce6decc3dfb47ab99e208302dd1520dccd3d0a9
Heraldlc <932172851@qq.com>
2021-08-23 20:09:56 +0800

    Update 416.分割等和子集.cpp

--- solutions/416.分割等和子集.cpp
+++ solutions/416.分割等和子集.cpp
@@ -34,3 +34,27 @@ public:
         return dp[n][sum];
     }
 };
+
+// 状态压缩后，改为一维数组
+
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        for(int num : nums) sum+=num;
+        if(sum%2 != 0) return false;
+        int n = nums.size();
+        sum = sum/2;
+        vector<bool> dp(sum+1, false);
+        dp[0] = true; // 背包空间为0，就是恰好装满
+
+        for(int i=0; i<n; i++){
+            for(int j=sum; j>=0; j--){
+                if(j-nums[i] >= 0) 
+                    dp[j] = dp[j] || dp[j-nums[i]];
+            }
+        }
+
+        return dp[sum];
+    }
+};
