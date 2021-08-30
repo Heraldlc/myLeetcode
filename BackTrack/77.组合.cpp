cecc1497099997f3b50d0d8b98e7938591222ba4
Healdlc <932172851@qq.com>
2021-08-30 19:07:14 +0800

    Update 77.组合.cpp

---
+++ BackTrack/77.组合.cpp
@@ -0,0 +1,28 @@
+// 比较明显的回溯思路
+// 定义backtrack()的时候，按照模拟过程一个一个确定参数即可
+
+class Solution {
+public:
+    vector<vector<int>> res;
+    void backtrack(int start, int k, int n, vector<int> &track){
+        // 长度达到了k就要返回了
+        if(track.size() == k){
+            res.push_back(track);
+            return ;
+        }
+        
+        for(int i=start; i<=n; i++){
+            track.push_back(i);
+            backtrack(i+1, k, n, track);
+            track.pop_back();
+        }
+    }
+
+    vector<vector<int>> combine(int n, int k) {
+        // base case
+        if(k <= 0 || n <=0) return res;
+        vector<int> track;
+        backtrack(1, k, n, track);
+        return res;
+    }
+};
